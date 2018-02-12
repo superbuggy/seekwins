ea5c981e419ec168ed4654e818629260c16a4c5d
James Reichard <jpqreichard@gmail.com>
2018-02-11 19:22:20 -0500

    add Tone Sequencer to state

--- src/state/index.js
+++ src/state/index.js
@@ -1,18 +1,10 @@
-import Tone from 'tone';
+import Tone from 'tone'
 
-const sequence = ["D#3", "D3", "G3", "A3", "A#3"]
-
-const toneSequence = new Tone.Sequence(
-  function (time, note) {
-    console.log(note);
-  }, 
-  sequence, 
-  '8n'
-);
-
+const sequence = ['D#3', 'D3', 'G3', 'A3', 'A#3']
 
 export default {
-  toneSequence,
   sequence,
+  toneSequence: null,
+  currentNote: sequence[0],
   transport: Tone.Transport,
 };
