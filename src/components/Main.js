e1c102b6282bf768af3dc6f0706630bfca1f5104
James Reichard <jpqreichard@gmail.com>
2018-02-11 19:21:40 -0500

    disable play/stop buttons during opposing play phase

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,20 +2,22 @@ import { h } from 'hyperapp';
 import Sequence from './Sequence';
 import Tone from 'tone';
 
-export default ({ sequence }, { start, stop }) => {
+export default ({ sequence, currentNote, toneSequence }, { start, stop }) => {
   console.log(sequence)
   return <div class="counter">
-    <Sequence sequence={sequence}/>
+    <Sequence sequence={sequence} currentNot={currentNote}/>
     <section>
       <button
         onclick={start}
-      >
+        disabled={toneSequence}
+        >
         ▶️
       </button>
       <button
+        disabled={!toneSequence}
         onclick={stop}
       >
-        ⏸
+        ⏹
       </button>
     </section>
   </div>;
