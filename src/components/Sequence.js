9db466a6d3cb29679bf62ce3d2cd8c87226c5ed7
James Reichard <jpqreichard@gmail.com>
2018-02-11 19:22:35 -0500

    add currentNote

--- src/components/Sequence.js
+++ src/components/Sequence.js
@@ -1,9 +1,10 @@
 import { h } from 'hyperapp';
 
-export default ({sequence}) => {
+export default ({sequence, currentNote}) => {
   return (
     <div>
       <h1>Notes</h1>
+      <code>{currentNote}</code>
       <code>{sequence.join(', ')}</code>
       <hr />
     </div>
