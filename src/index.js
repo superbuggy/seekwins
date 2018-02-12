a1a732e1bd0be36146a46d5ad3b9fd00296a2059
superbuggy <jpqreichard@gmail.com>
2018-02-12 11:33:14 -0500

    add redux devtools

--- src/index.js
+++ src/index.js
@@ -3,13 +3,26 @@ import actions from './actions'
 import state from './state'
 import view from './components/Main'
 import Tone from 'tone'
+import devtools from 'hyperapp-redux-devtools'
 
-const seekwins = app(
-  state,
-  actions,
-  view,
-  document.body,
-)
+
+let seekwins
+
+if (process.env.NODE_ENV !== 'production') {
+  seekwins = devtools(app)(
+    state,
+    actions,
+    view,
+    document.body,
+  )
+} else {
+  seekwins = app(
+    state,
+    actions,
+    view,
+    document.body,
+  )
+}
 
 
 const newSequence = (callback) => new Tone.Sequence(callback, seekwins.sequence(), '8n')
