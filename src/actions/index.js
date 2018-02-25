b23f8458fb78bd7fa7df1a9a3cb0e51007c048e4
superbuggy <jpqreichard@gmail.com>
2018-02-25 12:32:28 -0500

    Rename update function name

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,30 +1,13 @@
 import Tone from 'tone'
-import { start, stop } from '../index.js'
 
-export default {
-  start: () => ({ transport }) => {
-    start()
-    transport.start()
-    return { transport }
-  },
-  stop: () => ({ transport }) => {
-    stop()
-    transport.stop()
-    return { transport }
-  },
-  updateSequence: ({ oldNote, newNote }) => ({ sequence }) => {
-    const index = sequence.indexOf(oldNote)
-    console.log(oldNote, newNote, index)
-    return {
-      sequence: [
-        ...sequence.slice(0, index),
-        newNote,
-        ...sequence.slice(index + 1)
-      ]
-    }
-  },
-  add: (e, note) => ({ sequence }) => ({ sequence: [...sequence, note] }),
-  remove: (e, note) => ({ sequence }) => ({ sequence: sequence.filter(item => item !== note) }),
-  sequence: _ => ({sequence}) => sequence,
-  updateNote: newNote => _ => ({currentNote: newNote})
+import transportActions from './transport'
+import externals from './externals'
+import sequence from './sequence'
+
+const actions = {
+  ...transportActions,
+  ...externals,
+  ...sequence
 }
+
+export default actions
